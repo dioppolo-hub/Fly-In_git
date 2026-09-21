918d707d3ba97be55fff1193467730fc13577e65
dioppolo <diego.ioppolo2006@gmail.com>
2026-09-21 13:19:04 +0200

    funzioni di zones

--- Zones.py
+++ Zones.py
@@ -10,16 +10,58 @@ class Zone():
 		self.cost = 1
 		self.is_blocked = False
 		self.neighbours = set()
-	
-	def connect(self, other_zone, bidirectional=True):
+		self.drones = []
+
+	def is_zone_full(self) -> bool:
+		if len(self.drones) >= self.capacity:
+			return True
+		else:
+			return False
+
+	def can_enter_zone(self) -> bool:
+		if not self.is_blocked and not self.is_zone_full():
+			return True
+		else:
+			return False
+
+	def enter_zone(self, drone) -> bool:
+		if not self.can_enter_zone():
+			return False
+		self.drones.append(drone)
+		drone.x = self.x
+		drone.y = self.y
+		return True
+
+	def leave_zone(self, drone) -> bool:
+		if not drone in self.drones:
+			return False
+		self.drones.remove(drone)
+		return True
+
+	def connect_zones(self, other_zone, bidirectional=True) -> bool:
+		if other_zone == self:
+			return False
 		self.neighbours.add(other_zone)
 		if bidirectional:
 			other_zone.neighbours.add(self)
+		return True
 	
-	def disconnect(self, other_zone, bidirectional=True):
+	def disconnect_zones(self, other_zone, bidirectional=True) -> bool:
+		if other_zone == self:
+			return False
 		self.neighbours.discard(other_zone)
 		if bidirectional:
 			other_zone.neighbours.discard(self)
+		return True
+
+	def is_connected_to(self, other_zone) -> bool:
+		if other_zone in self.neighbours:
+			return True
+		else:
+			return False
+
+	def get_neighbours(self) -> set:
+		return self.neighbours.copy()
 
 	def get_zone_pos(self) -> tuple:
 		return (self.x, self.y)
@@ -27,11 +69,13 @@ class Zone():
 
 class Start_zone(Zone):
 	def __init__(self, x: int, y: int, name: str):
+		super().__init__(x, y, name)
 		self.capacity = 25
 
 
 class End_zone(Zone):
 	def __init__(self, x: int, y: int, name: str):
+		super().__init__(x, y, name)
 		self.capacity = 25
 
 
